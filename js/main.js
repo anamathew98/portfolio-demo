a5cd857ae8123c6addcfd64df8395abd2099d442
anamathew98 <anamathew98@gmail.com>
2023-06-29 19:53:44 -0500

    6/29

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-console.log()
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instance = M.Materialbox.getInstance(elems);
-   
-    instance.open();
-    instance.close();
-    instance.destroy();
-  });
- 
+
+
+ function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
+
+function toggleNav() {
+  document.getElementById('navWrapper').classList.toggle('active');
+  document.getElementById('navTrigger').classList.toggle('active');
+}
